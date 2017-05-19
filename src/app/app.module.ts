3c84f98abda0e2011330c8e72e0b194c745ab37b
Ralf Barkow <ralf.barkow@me.com>
2017-05-19 16:02:43 +0200

    Anders als jQuery, das nur als Hilfsmittel für Bootstrap benötigt wird, soll MomentJS im Programmcode direkt angesprochen werden. Komponenten wie app.module.ts, die MomentJS-Funktionen nutzen, müssen das Modul daher importieren:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+// MomentJS
+import * as moment from 'moment';
+import 'moment/locale/de';
+
 @NgModule({
   declarations: [
     AppComponent
